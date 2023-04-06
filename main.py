41744c13305fb2b5cbbb7fb1ccc705f76144bb97
elifaksoyy <32356435+elifaksoyy@users.noreply.github.com>
2023-04-06 14:42:17 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+vize = int(input("Vize notunuzu giriniz: "))
+final = int(input("Final notunuzu giriniz: "))
+ortalama = (vize * 0.4) + (final * 0.6) 
+  
+if final<40 or ortalama<50:
+    print("kullanıcı kaldı")
+
+if vize==final*2:
+    print("kullanıcı kaldı")
+else:
+    print("kullanıcı geçti")
+# eğer final 40'dan küçükse kullanıcı kaldı
+# eğer ortalama 50'den küçükse kullanıcı kaldı
+# eğer vize finalin 2 katı ise kullanıcı kaldı
+# bunun dışındaki tüm durumlarda kullanıcı geçti yazdırmak istiyoruz.
